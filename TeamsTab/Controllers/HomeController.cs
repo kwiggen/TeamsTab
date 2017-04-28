e016a88bed5908f19abcc39f90af45dbb8eb6613
wiggs@wiggenout.com <wiggs@wiggenout.com>
2017-04-28 09:38:35 -0700

    First Version

---
+++ TeamsTab/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TeamsTab.Models;
+
+namespace TeamsTab.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(string name, string tenant, string group, string entity, string locale)
+        {
+            var team = new TeamData();
+            team.User = name;
+            team.Tenant = tenant;
+            team.Group = group;
+            team.Entity = entity;
+            team.Locale = locale;
+            return View(team);
+        }
+        
+    }
+}
---
+++ TeamsTab/Models/TeamData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamsTab.Models
+{
+    public class TeamData
+    {
+        public TeamData()
+        {
+        }
+
+        public string User { get; set; }
+        public string Tenant { get; set; }
+        public string Group { get; set;}
+        public string Entity { get; set; }
+        public string Locale { get; set; }
+    }
+}
